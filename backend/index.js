5c5b05baf9f36a8cef903d870e5a73dbd619f90f
samlhuillier <sam.jansa123@gmail.com>
2021-04-06 17:08:58 +0100

    basic backend crud routes added

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-let users = [
-    {
-        id: 1,
-        name: 'sam',
-        email: 'sam@gmail.com',
-    },
-    {
-        id: 2,
-        name: 'jim',
-        email: 'jim@gmail.com',
-    },
-    {
-        id: 3,
-        name: 'dan',
-        email: 'dan@gmail.com',
-    },
-]
-app.get('/api/users', (req, res) => {
-    res.json(users)
-})
+//Parse the body:
+app.use(express.json())
 
+// app.use(express.urlencoded( { extended: false }))
+app.use('/api/users', require('./routes/api/users'))
 
 app.listen(PORT, ()=> console.log("server running on port " + PORT))
---
+++ backend/routes/api/users.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const uuid = require('uuid')
+const router = express.Router()
+
+let users = [
+    {
+        id: 1,
+        name: 'sam',
+        email: 'sam@gmail.com',
+    },
+    {
+        id: 2,
+        name: 'jim',
+        email: 'jim@gmail.com',
+    },
+    {
+        id: 3,
+        name: 'dan',
+        email: 'dan@gmail.com',
+    },
+]
+//get all users:
+router.get('/', (req, res) => {
+    res.json(users)
+})
+
+//Create a user:
+router.post('/', (req, res) => {
+    const newUser = req.body 
+    if (!newUser.email){
+        return res.status(400).json({ msg: 'Please include an email'})
+    }
+    else if (!newUser.name){
+        return res.status(400).json({ msg: 'Please include a name'})
+    }
+    newUser.id = uuid.v4()
+    users.push(newUser)
+    res.json(users)
+    
+})
+
+//Update user 
+router.put('/:id', (req, res) => {
+    // const foundUser = users.find(user => user.id===parseInt(req.params.id))
+    
+    users.map(user => {
+        if (user.id===parseInt(req.params.id)){
+            if (req.body.name){
+                user.name = req.body.name
+            }
+            if (req.body.email){
+                user.email = req.body.email 
+            }    
+        }
+        return user
+    })
+    console.log(users)
+    res.json(users)
+})
+
+router.delete('/:id', (req, res) => {
+    // const foundUser = users.find(user => user.id===parseInt(req.params.id))
+    console.log(parseInt(req.params.id))
+    users.filter(user => user.id !== parseInt(req.params.id))
+    res.json(users)
+})
+
+module.exports = router
